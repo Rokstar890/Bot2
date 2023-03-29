780de745a2ce48ae7d3e32ce3545547052518e87
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-03-29 13:46:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
-API_ID       = 21723146
-API_HASH     = "07cd9c82699c28111cb33693ecbd9116"
-BOT_TOKEN    = "6264487502:AAHZMluG6JYyJ_2SVuYDYispJjoM8jeER3o"
+API_ID       = 16445683
+API_HASH     = "d0852e13eee2389ff2d9183b00649547"
+BOT_TOKEN    = "6195937798:AAGjRhA7TtkGAC6x2ftVNCRKOCo7FOH1_rU"
 SESSION      = "BQD68PMAWLPtBGZBIT8-eTB8qZpUZ-U0L4DLSHxqvcDgT2NmmmeTTlLj9sEtWQMRRsyqtlg8RNJFKwjHxw1N-nhFf7MoDZ_ZNI54aPiGNgyP60DghbyJM5E5VEVyt8efXAcH3T-kBVMKyxJa_MHaEasuHF5MC9sq5WH7IynjGdzdabQR_zaU__OZnfDAHL6stFSEdkA_XAIeMtxP_P8oz0TY2ygdB_y2uRjy54KYmzb4vuFBt7s8eEvu1gQ8_Aw3Gcde3BqEKKaAFcOCajGeCDEPXKTC26b41uBJUOaovrxME87iYaucHjr8agYAR_dYX48hHX29ND5IFP9yVaBNnyNtkk8PNQAAAAFs53EdAA"
-DATABASE_URI = "mongodb+srv://abdul:abdul@cluster0.tz24m3k.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001720210775
-ADMIN        = 6133440326
-CHANNEL      = "-1001836465539"
+DATABASE_URI = "mongodb+srv://SawanSingh:sawansingh@cluster0.h2prshr.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001765670990
+ADMIN        = 5651594253
+CHANNEL      = "-1001765670990"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
