7d1e1564a4eca2798507abbea29f6de7164bd586
Nothing bro ✓ <125857863+Singhsawan@users.noreply.github.com>
2023-04-07 20:04:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,7 +2,7 @@ API_ID       = 16445683
 API_HASH     = "d0852e13eee2389ff2d9183b00649547"
 BOT_TOKEN    = "6195937798:AAGjRhA7TtkGAC6x2ftVNCRKOCo7FOH1_rU"
 SESSION      = "BQD68PMAsUuiQekNe2Hug_bY4aLd7P0qBjB5C4nqnrlyZNl1k-8S01TL_-Hu6ayW0zSBZj31Ej74AsHCzUpudoKsMJ7chBQUBGpk8X7zzw0qHlrTRr4_tNY16lL8p64lD_EI3aU8AUKukeF3z3OtEPYhXkDr-11kFSTvcKWsl10HmQJunfUixERT6EMDCAJ0jGF-4CyIp3KVaRHjjJ0AhCiHonK1F59fqW032-pStVV08iHwuA3sFmaouJWt_Wq3AREn7O6SUxf_e-V7Um9FMd-8qebY6H_5bz2zgBnCh6VYPlOv9SoWkSHoKqWWtaix1KValRzrvgnZLPxEeIHyIOUdisTuwQAAAAFm2wNIAA"
-DATABASE_URI = "mongodb+srv://SawanSingh:sawansingh@cluster0.h2prshr.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://herokudeploye:herokudeploye@cluster0.m9r3mk8.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001765670990
 ADMIN        = 5651594253
 CHANNEL      = "-1001765670990"
