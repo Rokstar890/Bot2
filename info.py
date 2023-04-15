469984a8349a24d88fc08bc6a698b7400c8de35b
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2023-04-15 14:51:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
-API_ID       = 16445683
-API_HASH     = "d0852e13eee2389ff2d9183b00649547"
-BOT_TOKEN    = "6195937798:AAGjRhA7TtkGAC6x2ftVNCRKOCo7FOH1_rU"
-SESSION      = "BQD68PMAodk9mZoHZt8Zno-ODIoxvMe_RsTSsoY_ZJtojCr4En1l8awvBQmzh-pqVzvINRHVslYT2KejJ3XEZL_KA0twKdXpxR9uHRlZiPNFZYAsaq7EVAP--lcsZABiZdZomU2kDW8xbQ2zMF2iRQlzlEcbvjWYo99fE12ITJt7-lst2y5Y5XpCrs51JsI0IvaBzDH5T04a3RQl4dIoDi1Y6HL8BAICxEZDwLHQqtse0_2e4o6j8loBixns8_4LBhNkBDPY4mmMxzDOHrXa4csUICnaW8c8YcoT2tZB3hoDSSltAhqS1kl9c8lqNm0SsAjLjQ5Di92dFCpXgIi0qeJ6FSbRRQAAAAFm2wNIAA"
-DATABASE_URI = "mongodb+srv://Sawansingh24:Sawansingh24@cluster0.uiuhxxj.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1001765670990
-ADMIN        = 5651594253
-CHANNEL      = "-1001765670990"
+API_ID       = 10753388
+API_HASH     = "bc2a8c10eada9c42315e869302c6cbc5"
+BOT_TOKEN    = "5955187646:AAGWdsUZ1rzWkavakxmr1DwV2REq8D4twQA"
+SESSION      = "BQBuhRathmYzyxtm4TGhzYsvE7xW_45omRZaEXn4CwIi2fsOpjhz2AHqCYHodViMKcrWm4WPv-luo4vxmL-ZWdKqyJNkM1_DsfIaYHfACKUKmN9yrc4tN7t7J9m127UdTS6Kwx4FHbsday-b08vrAeyzXBpQNEEsogUrXmSXgx5202M5Xw_Lrc8N1aApc9b-Ac7Vukc64mGu6Dp3ZdK6sKsgsR5qafE3Dri7wuN3cLCppj5UVoOgE5wp7FBP281VidzgjJYlxdCjHDUJgMDAWiW6lSyvFi8QC3iDn8Xg6PfrPa9mMu4WB5_QUKQTOc7-L8AqGFKRONQNc4g--ABTDcnidJyTLgA"
+DATABASE_URI = "mongodb+srv://Advancefilter:mdisk@cluster0.lfk69gf.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001196643845
+ADMIN        = 966029924
+CHANNEL      = "-1001972619094"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
