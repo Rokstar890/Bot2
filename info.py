69f3cb4c256c96e143ac46c9a1b6ed3f5efef017
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2023-04-15 15:36:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,10 @@
-API_ID       = 10753388
+API_ID       = "10753388"
 API_HASH     = "bc2a8c10eada9c42315e869302c6cbc5"
 BOT_TOKEN    = "5955187646:AAGWdsUZ1rzWkavakxmr1DwV2REq8D4twQA"
 SESSION      = "BQBuhRathmYzyxtm4TGhzYsvE7xW_45omRZaEXn4CwIi2fsOpjhz2AHqCYHodViMKcrWm4WPv-luo4vxmL-ZWdKqyJNkM1_DsfIaYHfACKUKmN9yrc4tN7t7J9m127UdTS6Kwx4FHbsday-b08vrAeyzXBpQNEEsogUrXmSXgx5202M5Xw_Lrc8N1aApc9b-Ac7Vukc64mGu6Dp3ZdK6sKsgsR5qafE3Dri7wuN3cLCppj5UVoOgE5wp7FBP281VidzgjJYlxdCjHDUJgMDAWiW6lSyvFi8QC3iDn8Xg6PfrPa9mMu4WB5_QUKQTOc7-L8AqGFKRONQNc4g--ABTDcnidJyTLgA"
 DATABASE_URI = "mongodb+srv://Advancefilter:mdisk@cluster0.lfk69gf.mongodb.net/?retryWrites=true&w=majority"
 LOG_CHANNEL  = -1001196643845
-ADMIN        = 966029924
+ADMIN        = "966029924"
 CHANNEL      = "-1001972619094"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
