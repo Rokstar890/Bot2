686f1667414859fe522b5a93f6f25567fd99d71e
Ps705112 <96942532+Ps705112@users.noreply.github.com>
2023-04-15 20:13:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,10 @@
-API_ID       = "10753388"
-API_HASH     = "bc2a8c10eada9c42315e869302c6cbc5"
-BOT_TOKEN    = "5955187646:AAGWdsUZ1rzWkavakxmr1DwV2REq8D4twQA"
-SESSION      = "BQECh0kApWjSG6ltF-LU_hLBOXoSAeGwUXQXnqmdBj75xZxhyEVf1Dd2GmAqt3C1pT2rReL6CN6MCPU4Tvq8B47-y1gJG4LAFjnX7qoR55L17zRD3Jlx-QVrtAVEI6PRhW48E_fYnThoYDFY-6C_b1Uq0bdkPZT35dtQdKuuflBKgYqtpUoNcPIcIDl7_z6C-nfbvlO5nM32p0fTJiPCFeQG6P64Kj51f0LJK4HT6NUrK2SeqOz92xbQiu2lldK457fLyADOD9yuvM9prLmP6reyP_YetlT4dMsdzS32MGJr-gevJdXLmXkw_dAKztUSw403cM6RE-2nTo8gbiBUTbLpD_U6mwAAAAB0nJMuAA"
-DATABASE_URI = "mongodb+srv://Test:mdisk@cluster0.1hafe7j.mongodb.net/?retryWrites=true&w=majority"
+import os
+
+API_ID       = os.environ.get("API_ID", "10753388")
+API_HASH     = os.environ.get("API_HASH", "bc2a8c10eada9c42315e869302c6cbc5")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "5955187646:AAGWdsUZ1rzWkavakxmr1DwV2REq8D4twQA")
+SESSION      = os.environ.get("SESSION", "BQECh0kApWjSG6ltF-LU_hLBOXoSAeGwUXQXnqmdBj75xZxhyEVf1Dd2GmAqt3C1pT2rReL6CN6MCPU4Tvq8B47-y1gJG4LAFjnX7qoR55L17zRD3Jlx-QVrtAVEI6PRhW48E_fYnThoYDFY-6C_b1Uq0bdkPZT35dtQdKuuflBKgYqtpUoNcPIcIDl7_z6C-nfbvlO5nM32p0fTJiPCFeQG6P64Kj51f0LJK4HT6NUrK2SeqOz92xbQiu2lldK457fLyADOD9yuvM9prLmP6reyP_YetlT4dMsdzS32MGJr-gevJdXLmXkw_dAKztUSw403cM6RE-2nTo8gbiBUTbLpD_U6mwAAAAB0nJMuAA")
+DATABASE_URI = os.environ.get("DATABASE_URI","mongodb+srv://Test:mdisk@cluster0.1hafe7j.mongodb.net/?retryWrites=true&w=majority")
 LOG_CHANNEL  = -1001196643845
 ADMIN        = "966029924"
 CHANNEL      = "-1001972619094"
