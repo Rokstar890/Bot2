d7fd331fb7f210b0a8f12df62714187736f11c36
Rokstar890 <144723163+Rokstar890@users.noreply.github.com>
2023-10-04 21:15:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,15 +1,15 @@
 import os
 
-API_ID       = os.environ.get("API_ID", "10753388")
-API_HASH     = os.environ.get("API_HASH", "bc2a8c10eada9c42315e869302c6cbc5")
-BOT_TOKEN    = os.environ.get("BOT_TOKEN", "5955187646:AAGWdsUZ1rzWkavakxmr1DwV2REq8D4twQA")
-SESSION      = os.environ.get("SESSION", "BQECh0kApWjSG6ltF-LU_hLBOXoSAeGwUXQXnqmdBj75xZxhyEVf1Dd2GmAqt3C1pT2rReL6CN6MCPU4Tvq8B47-y1gJG4LAFjnX7qoR55L17zRD3Jlx-QVrtAVEI6PRhW48E_fYnThoYDFY-6C_b1Uq0bdkPZT35dtQdKuuflBKgYqtpUoNcPIcIDl7_z6C-nfbvlO5nM32p0fTJiPCFeQG6P64Kj51f0LJK4HT6NUrK2SeqOz92xbQiu2lldK457fLyADOD9yuvM9prLmP6reyP_YetlT4dMsdzS32MGJr-gevJdXLmXkw_dAKztUSw403cM6RE-2nTo8gbiBUTbLpD_U6mwAAAAB0nJMuAA")
-DATABASE_URI = os.environ.get("DATABASE_URI","mongodb+srv://Test:mdisk@cluster0.1hafe7j.mongodb.net/?retryWrites=true&w=majority")
-LOG_CHANNEL  = -1001196643845
-ADMIN        = "966029924"
-CHANNEL      = "-1001972619094"
+API_ID       = os.environ.get("API_ID", "23762978")
+API_HASH     = os.environ.get("API_HASH", "eff30dac5504a8660e69bfe19f668571")
+BOT_TOKEN    = os.environ.get("BOT_TOKEN", "6335802263:AAFB1gPJQDTtYfXkbi0mmLvYlDcXbEUHiYo")
+SESSION      = os.environ.get("SESSION", "BQFqmCIAgFtGFzKdrIYsALZcExSDuMcwynDZf5Eu1ol_zmVfkfIo4Vp9NeLwtP4BnYrr-FJy5IKhcp9ZgR1ZhtJLZxi-fgvWYkNk6FA7u8K8ppZ_eXptdNLLUhWvnK4sRoW7Ta4VWkh8ZPla0nlQ6oNldNXHGq7I9r7RDMJWBIJw5wkULk1aROkT7bQGkiyYyMRRnx0UF390bZzhgm1CVwtk_eydRcz9coK2s0Z-WnRIlKu2IUwn3t3Eq9EdNgfNDlZpNXF8MMOMn_iee7bmv0f6dcgPLl4OoUjSmay7OLsRxaTWooCrPULGQpRVnEJ7qtUlBV54vPZ5Lz3V7gwv0hJV1OjCkwAAAABIf2IgAA")
+DATABASE_URI = os.environ.get("DATABASE_URI","mongodb+srv://hebapir376:ravi1234@cluster0.x9pmpfx.mongodb.net/?retryWrites=true&w=majority")
+LOG_CHANNEL  = -1001940288450
+ADMIN        = "1216307744"
+CHANNEL      = "-1001394587233"
 APPROVETEXT  = "Hello {mention}\nWelcome To {title}\n\nYour request has been approved"
 APPROVE     = "on"
-OWNER_USERNAME = "pankajpandiyar"
+OWNER_USERNAME = "netflix_india_007"
 UPI_PIC = "https://telegra.ph/file/3eded98acd6835f6aa4e6.jpg"
-BOT_USERNAME = ""
+BOT_USERNAME = "Netflixbot_movie_bot"
